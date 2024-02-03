6f61359ef0b2d3636db67edbd17b1356941860ed
Maxim Kutprot <maksim.kutprot@daubit.org>
2024-02-03 22:50:20 +0100

    info7

--- src/bot.mjs
+++ src/bot.mjs
@@ -3,17 +3,6 @@ import TeleBot from "telebot";
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
 // bot.on("text", (msg) => msg.reply.text(msg.text));
-
-var options = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{ text: "Алиса мини 🟣", callback_data: "1" }],
-      [{ text: "Алиса миди 🟠", callback_data: "data 2" }],
-      [{ text: "Алиса макси 🟢", callback_data: "text 3" }],
-    ],
-  }),
-};
-
 bot.on(["/info", "/hello"], (msg) => msg.reply.text("Welcome!"));
 
 export default bot;
