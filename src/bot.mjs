49a3e9d34a51f192df3d97280c1a2a5bb7c4cac5
Maxim Kutprot <maksim.kutprot@daubit.org>
2024-02-03 22:21:04 +0100

    info

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,7 +1,15 @@
-import TeleBot from "telebot"
+import TeleBot from "telebot";
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on("text", msg => msg.reply.text(msg.text))
+// bot.on("text", (msg) => msg.reply.text(msg.text));
 
-export default bot
+bot.onText(/\/info/, function (msg) {
+  bot.sendMessage(
+    msg.chat.id,
+    "Что вас конкретно интересует? Выберите варианты ниже:",
+    options
+  );
+});
+
+export default bot;
