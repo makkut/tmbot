1835c949df7a17bb55f682cebcdda874aa12454d
Maxim Kutprot <maksim.kutprot@daubit.org>
2024-02-04 11:28:25 +0100

    info15

--- src/bot.mjs
+++ src/bot.mjs
@@ -2,8 +2,8 @@ import TeleBot from "telebot";
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on("text", (msg) => msg.reply.text(msg.text));
-// bot.on(["/info", "/hello"], (msg) => msg.reply.text("Welcome!"));
+// bot.on("text", (msg) => msg.reply.text(msg.text));
+bot.on(["/info", "/hello"], (msg) => msg.reply.text("Welcome!"));
 
 // bot.on(/\/info/, function (msg) {
 //   return bot.sendMessage(
