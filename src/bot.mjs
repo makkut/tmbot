ed609b04ebc99240b411a44b17d618356277257d
Maxim Kutprot <maksim.kutprot@daubit.org>
2024-02-04 21:15:48 +0100

    text11

--- src/bot.mjs
+++ src/bot.mjs
@@ -73,14 +73,10 @@ bot.on("/inlineKeyboard", (msg) => {
 
 // Inline button callback
 bot.on("callbackQuery", async (msg) => {
-  let replyMarkup = bot.sendMessage(
-    "Яндекс станция Дуо Макс (https://alice.yandex.ru/station-duo-max#cta-section) за 449€ + 10€ пересылка по Германии с номером отслеживания! Доступные цвета: Чёрная и Бежевая, ждём Красную и Зелёную. По заказу пишем сюда: @andreslarge"
-  );
-  return await bot.sendVideo(
-    msg.from.id,
-    "https://yastatic.net/s3/lpc-ext/Alice_website/Chiron/7.12/hero-NY-pc.mp4",
-    { replyMarkup }
-  );
+  //   return await bot.sendVideo(
+  //     msg.from.id,
+  //     "https://yastatic.net/s3/lpc-ext/Alice_website/Chiron/7.12/hero-NY-pc.mp4"
+  //   );
   // User message alert
   //   return bot.answerCallbackQuery(
   //     msg.id,
